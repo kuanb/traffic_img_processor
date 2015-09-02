1c3daa95aeee796d58d293c0a87600545abb6c00
Kuan Butts <kuanbutts@yahoo.com>
2015-09-02 17:52:28 -0400

    completed mechanism seems to be working on images

--- median.py
+++ median.py
@@ -1,5 +1,7 @@
 import cv2
 import os
+import numpy
+import math
 
 allPhotos = []
 
@@ -12,14 +14,24 @@ for fn in os.listdir(path):
 
 # check that all images are the same size
 if all(x.shape == allPhotos[0].shape for x in allPhotos):
+    col = range(allPhotos[0].shape[0]-1)
+    row = range(allPhotos[0].shape[1]-1)
+    for c in col:
+        for r in row:
+            eachImageVals = {"r": [], "b": [], "g":[]}
+            for img in allPhotos:
+                vals = img[c,r]
+                eachImageVals["r"].append(vals[0])
+                eachImageVals["b"].append(vals[1])
+                eachImageVals["g"].append(vals[2])
+            for v in eachImageVals:
+                eachImageVals[v] = numpy.median(numpy.array(eachImageVals[v]))
+                eachImageVals[v] = math.trunc(eachImageVals[v])
+            allPhotos[0][c,r] = [eachImageVals["r"],eachImageVals["g"],eachImageVals["b"]]
 
-for img in allPhotos:
-    print img.shape
+print 'finished'
 
-# img = cv2.imread('0.jpg')
-#
-# px = img[100,100]
-# print px
+cv2.imshow('image', allPhotos[0])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-# plt.imshow(img)
-# plt.show()
